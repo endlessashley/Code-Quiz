0a091df259744170da0c48b3f23359d9241788d4
endlessashley <81969558+endlessashley@users.noreply.github.com>
2021-06-26 23:45:21 -0400

    unsuccessfully coding score and highscore list

--- assets/script.js
+++ assets/script.js
@@ -1,136 +1,242 @@
-const startButton = document.getElementById("start-btn")
-const nextButton = document.getElementById("next-btn")
-const questionContainerElement = document.getElementById("question-container")
-const questionElement = document.getElementById("question")
-const answerButtonsElement = document.getElementById("answer-buttons")
-let shuffledQuestions, currentQuestionIndex
+var timer = 0
+var score = 0
+var timerCount
+var numCorrect = 0
+var timerElement = document.querySelector(".timer-count")
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 const questions = [
-    {
-    question: "He's just a little rabbit. What's he gonna do?",
-      answers: 
-      [
-        {text: "Bite your finger", correct: false},
-        {text: "Nibble your bum", correct: true},
-        {text: "Eat a carrot", correct: false},
-        {text: "None of the above", correct: false},
-      ]
-    },
-   
-    {
-      question: "Where we're going, do we need roads?",
-      answers: 
-        [
-          {text: "No", correct: true},
-          {text: "Yes", correct: false},
-          {text: "Both", correct: false},
-          {text: "Neither", correct: false},
-        ]
-    },
-      
     {
       question: "What's in the box?",
       answers: [
-          {text: "Brad Pitt's head", correct: false},
-          {text: "A Tiffany vase", correct: false},
-          {text: "Gwyneth Paltrow's head", correct: true},
-          {text: "A sandwich", correct: false},
-        ]
-      },
-
-    {
-      question: "Arent' you a little short for a storm trooper?",
-      answers: [
-          {text: "Listen, my height is average for a Tatooine farmboy.", correct: false},
-          {text: "*bursts into tears*", correct: false},
-          {text: "Not where it counts!", correct: false},
-          {text: "Huh? Oh, the uniform.", correct: true},
+        { text: "Gwyneth Paltrow's Head", correct: true },
+        { text: "Brad Pitt's head", correct: false },
+        { text: "*bursts into tears*", correct: false },
+        {text: "A sandwich", correct: false },
       ]
     },
-
+    {
+      question: "Aren't you a little short for a Stormtrooper?",
+      answers: [
+        { text: "*bursts into tears*", correct: false },
+        { text: "I'm perfectly average for a Tatooine farmboy", correct: false },
+        { text: "Not where it counts!", correct: false },
+        { text: "Huh? Oh, the uniform", correct: true }
+      ]
+    },
     {
       question: "Does this rug really tie the room together?",
       answers: [
-          {text: "Yes; so much, in fact, that it kicks off the plot of an entire movie", correct: true},
-          {text: "I guess so, if you squint", correct: false},
-          {text: "Rugs are in the eye of the beholder", correct: false},
-          {text: "This rug is unremarkable in every way", correct: false},
-      ] 
+        { text: "This rug is unremarkable in every way", correct: false },
+        { text: "Obviously. It kicks off the plot of an entire movie", correct: true },
+        { text: "*bursts into tears*", correct: false },
+        { text: "Rugs are in the eye of the beholder", correct: false }
+      ]
+    },
+    {
+      question: "Can you handle the truth?",
+      answers: [
+        { text: "I'll handle your face", correct: false },
+        { text: "God never gives you anything you can't handle", correct: false },
+        { text: "You can't handle the truth!", correct: true },
+        { text: "*bursts into tears*", correct: false },
+      ]
     }
-]
+  ]
 
+let shuffledQuestions, currentQuestionIndex
 
-startButton.addEventListener("click", startGame)
-nextButton.addEventListener("click", () => {
-    currentQuestionIndex++
-    setNextQuestion()
-
+startButton.addEventListener('click', startGame)
+//proceeds to next random question
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
 })
 
 function startGame() {
-    console.log("Started")
-    startButton.classList.add("hide")
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainerElement.classList.remove("hide");
-    setNextQuestion()
+  startButton.classList.add('hide')
+  //to produce questions randomly
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  startTimer()
+  setNextQuestion()
 }
 
-function setNextQuestion (){
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+function startTimer() {
+    timerCount = 90;
+          var timerInterval = setInterval(function() {
+            timerCount--;
+            timerElement.textContent = timerCount;
+            
+            if (timerCount <= 0) {
+                clearInterval(timerInterval);
+                timerCount = 0
+                endQuiz ();
+            }
+         
+            else if (question === currentQuestionIndex.length) {
+              clearInterval(timer);
+              endquiz();
+            }
+
+            else {timerCount--; }
+          
+          }, 1000);
+} 
+
+function endQuiz(){
+  $('#score').text(timerCount);
 }
 
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+//to populate question container with question text and answer options
 function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement("button")
-        button.innerText = answer.text
-        button.classList.add("btn")
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
 }
 
+//to clear previous questions/answers before next question presented
 function resetState() {
-    nextButton.classList.add("hide")
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(
-            answerButtonsElement.firstChild)
-    }
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
 }
+
+//handling functionality when user selects an answer
 function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    nextButton.classList.remove("hide")
-    if (shuffledQuestions.length > currentQuestionIndex + 1){
-        nextButton.classList.remove("hide")
-    }
-    else {
-        startButton.innerText = "Restart"
-        startButton.classList.remove("hide")
-    }
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+  checkResult()
+}
 
+function checkResult (correct) {
+    if (correct == false) {
+        timerCount -= 10
+    }
+}
+
+//change incorrect answers to red and correct answers to blue
 function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add("correct")
-    } else {
-        element.classList.add("wrong")
-    }
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
 }
 
+//to return previous questions' correct/incorrect responses to neutral color for the next question
 function clearStatusClass(element) {
-    element.classList.remove("correct")
-    element.classList.remove("wrong")
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
 
+//high score form
+
+function displayHighscores() {
+  //getting things out of local storage
+  var values = [],
+  keys = Object.keys(localStorage),
+  i = keys.length;
+
+  while(i--) {
+      values.push(JSON.parse(localStorage.getItem(keys[i])))
+  }
+
+  // order from highest score to lowest
+  values.sort((a, b) => (a.score > b.score) ? -1 : 1)
+
+  console.log(values);
+  for(var i = 0; i < values.length; i++) {
+      // Creates list item for entries in local storage
+      $('#highscoreList').append("<li class='score'>" + values[i].initials + " - " + values[i].score + "</li>")
+  }
 }
+$('#submitBtn').click(function(event) {
+      event.preventDefault();
+      // save timeTracker as score in an object
+      var user = {
+      initials: $('#initials').val(),
+      score: timerCount
+      };
+
+      // put the object in localStorage, use input as key
+      localStorage.setItem(JSON.stringify(initials.value), JSON.stringify(user));
+  }
+)
 
 
+//var emailInput = document.querySelector("#email");
+//var passwordInput = document.querySelector("#password");
+//var signUpButton = document.querySelector("#sign-up");
+//var msgDiv = document.querySelector("#msg");
+//var userEmailSpan = document.querySelector("#user-email");
+//var userPasswordSpan = document.querySelector("#user-password");
+
+
+//renderLastRegistered();
+
+//function displayMessage(type, message) {
+  //msgDiv.textContent = message;
+  //msgDiv.setAttribute("class", type);
+//}
+
+//function renderLastRegistered() {
+  //var email = localStorage.getItem("email");
+  //var password = localStorage.getItem("password");
+
+  //if (!email || !password) {
+    //return;
+  //}
+
+  //userEmailSpan.textContent = email;
+  //userPasswordSpan.textContent = password;
+//}
+
+///signUpButton.addEventListener("click", function(event) {
+ //event.preventDefault();
+
+  //var email = document.querySelector("#email").value;
+  //var password = document.querySelector("#password").value;
+
+  //if (email === "") {
+    //displayMessage("error", "Email cannot be blank");
+  //} else if (password === "") {
+    //displayMessage("error", "Password cannot be blank");
+  //} else {
+    //displayMessage("success", "Registered successfully");
+
+    //localStorage.setItem("email", email);
+    //localStorage.setItem("password", password);
+    //renderLastRegistered();
+  //}
+//});
